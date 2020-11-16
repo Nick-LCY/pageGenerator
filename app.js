74e61fffc7459ff4c6c4494d436b827919e66832
Nick Lin <nicklin9907@gmail.com>
2020-11-16 17:04:00 +0800

    connect to mysql database

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ const fs = require("fs")
 const express = require("express")
 const parseVarsToObjs = require("./processingUtils/generatePaperUtils/parseVarsToObj.cjs")
 const generateVars = require("./processingUtils/generatePaperUtils/generateVars.cjs")
+const connection = require("./database/connectToDatabase.cjs")
 
 const app = express()
 const port = 8080
 
+
+connection.connect();
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
 app.get('/process', async (req, res) => {
-    var file = JSON.parse(await fs.readFileSync("multiple.json"))
-    var varSpecificationList = parseVarsToObjs(file.vars)
+    var name = "Hello World"
+    // var file = JSON.parse(await fs.readFileSync("multiple.json"))
 
-    res.send(generateVars(varSpecificationList))
+    var file, varSpecificationList
+    connection.query(`select content from specifications where name = '${name}'`, (err, rows, fields) => {
+        if (err) throw err
+        file = JSON.parse(rows[0].content)
+        varSpecificationList = parseVarsToObjs(file.vars)
+        res.send(generateVars(varSpecificationList))
+    })
+    console.log("finished!")
 })
 
 app.listen(port, () => {
---
+++ database/connectToDatabase.cjs
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const host = "localhost"
+const database = "fyp"
+const user = "root"
+const password = "root"
+const port = "3306"
+
+var connection = mysql.createConnection({
+    host,
+    user,
+    password,
+    port,
+    database
+})
+
+module.exports = connection
